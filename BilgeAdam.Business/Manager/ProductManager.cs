ec7a90c838ae880d335c6c6a0ce0693e4c7e74b1
cagatay.yildiz <cagatay.yildiz@BSKPC103-YZLM>
2019-05-05 13:53:47 +0300

    productmanager update

--- BilgeAdam.Business/Manager/ProductManager.cs
+++ BilgeAdam.Business/Manager/ProductManager.cs
@@ -2,6 +2,7 @@
 using BilgeAdam.Data.ORM.Entity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,13 +33,110 @@ namespace BilgeAdam.Business.Manager
             }
         }
 
-
-        public List<Product> GetAllProductsWithCount(int count)
+        public static List<Product> GetAllProductsWithCount(int count)
         {
             using (BilgeAdamContext db = new BilgeAdamContext())
             {
                 return db.Products.Take(count).ToList();
             }
         }
+
+        //Dışarıdan alınan ID ye göre ürünü dönen metot
+        public static Product GetProductbyID(int id)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                return db.Products.FirstOrDefault(x => x.ID == id && x.IsDeleted == false);
+            }
+        }
+
+        //Dışarıdan aldığu stringe göre ürün adında o kelime geçen ÜRÜNLER
+        public static List<Product> GetAllProductsbySearchProductName(string name)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                return db.Products.Where(q => q.ProductName.Contains(name)).ToList();
+            }
+        }
+
+        //En pahalı ürün
+        public static Product GetProductMaxUnitPrice()
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                //test edilecek
+                return db.Products.OrderBy(q => q.UnitPrice).Max();
+
+
+                return db.Products.OrderBy(q => q.UnitPrice).Take(1).FirstOrDefault();
+            }
+        }
+
+        //ürünün kategorisinin adı dışarıdan gelen string ile başlayan ÜRÜNLER
+        public static List<Product> GetAllProductsbyCategoryName(string categoryname)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                //patlayabilir !
+                return db.Products.Where(q => q.Category.CategoryName.StartsWith(categoryname)).ToList();
+            }
+        }
+
+        //dışarıdan alınan isme göre ürün var mı?
+        public static bool CheckProductwithProductName(string name)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                bool result = db.Products.Any(q => q.ProductName == name);
+
+                return result;
+
+                //Product product = db.Products.FirstOrDefault(q => q.ProductName == name);
+
+                //if (product != null)
+                //    return true;
+                //else
+                //    return false;
+            }
+        }
+
+        //update metodu
+        public static void UpdateProduct(Product product)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+
+        }
+
+        //id ye göre HARD silme metodu
+        public static void HardDeleteProduct(int id)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                Product product = db.Products.FirstOrDefault(q => q.ID == id);
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+        }
+
+        //idye göre silme
+        public static void DeleteProduct(int id)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                Product product = db.Products.FirstOrDefault(q => q.ID == id);
+                product.IsDeleted = true;
+                product.DeleteDate = DateTime.Now;
+
+                db.SaveChanges();
+                
+            }
+        }
+
+
+
     }
 }
