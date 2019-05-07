bfc30a01638f7605cfb1a6a8f422e6278c63857e
Doruk Yeşil <dorukyesil@hotmail.com>
2019-05-07 20:37:52 +0300

    CategoryManager sınıfı açıldı, Get All ve Add metodu yazıldı.

---
+++ BilgeAdam.Business/Manager/CategoryManager.cs
@@ -0,0 +1,36 @@
+﻿using BilgeAdam.Data.ORM.Context;
+using BilgeAdam.Data.ORM.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.Business.Manager
+{
+    class CategoryManager
+    {
+        public static List<Category> GetAllCategories()
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+
+        public static void AddCategory(Category category)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                category.AddDate = DateTime.Now;
+                category.IsDeleted = false;
+                category.UpdateDate = DateTime.Now;
+
+                db.Categories.Add(category);
+
+                db.SaveChanges();
+            }
+        }
+
+    }
+}
