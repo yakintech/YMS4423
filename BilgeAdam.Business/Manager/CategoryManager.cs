98551f49237b1828540182093630249687bda8d4
Doruk Yeşil <dorukyesil@hotmail.com>
2019-05-07 21:20:53 +0300

    sprint completed

--- BilgeAdam.Business/Manager/CategoryManager.cs
+++ BilgeAdam.Business/Manager/CategoryManager.cs
@@ -2,13 +2,14 @@
 using BilgeAdam.Data.ORM.Entity;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BilgeAdam.Business.Manager
 {
-    class CategoryManager
+    public class CategoryManager
     {
         public static List<Category> GetAllCategories()
         {
@@ -29,6 +30,42 @@ namespace BilgeAdam.Business.Manager
                 db.Categories.Add(category);
 
                 db.SaveChanges();
+            }
+        }
+
+        public static List<Category> GetAllActiveCategories()
+        {
+            var data = GetAllCategories().Where(x => x.IsDeleted == false).ToList();
+
+            return data;
+        }
+
+        public static void UpdateCategory(Category category)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public static void DeleteCategory(int id)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                Category deleteddata = db.Categories.FirstOrDefault(x => x.ID == id);
+                deleteddata.IsDeleted = true;
+                deleteddata.DeleteDate = DateTime.Now;
+
+                db.SaveChanges();
+            }
+        }
+
+        public static List<Category> SearchMethod(string data)
+        {
+            using (BilgeAdamContext db = new BilgeAdamContext())
+            {
+                return db.Categories.Where(x => x.CategoryName.Contains(data)).ToList();
             }
         }
 
---
+++ BilgeAdam.UI.Web/Models/VM/CategoryVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BilgeAdam.UI.Web.Models.VM
+{
+    public class CategoryVM
+    {
+        [Required(ErrorMessage = "Kategori adı boş geçilemez")]
+        [Display(Name = "Kategori Adı")]
+        public string KategoriAdi { get; set; }
+
+        [Display(Name = "Açıklama")]
+        public string Aciklama { get; set; }
+
+    }
+}
