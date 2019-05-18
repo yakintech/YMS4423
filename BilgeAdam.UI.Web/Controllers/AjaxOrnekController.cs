c1d428f788d8192cd4811db220c50196b626ac80
cagatay.yildiz <cagatay.yildiz@BSKPC103-YZLM>
2019-05-18 12:55:28 +0300

    ajax test

---
+++ BilgeAdam.UI.Web/Controllers/AjaxOrnekController.cs
@@ -0,0 +1,18 @@
+﻿using BilgeAdam.UI.Web.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BilgeAdam.UI.Web.Controllers
+{
+    public class AjaxOrnekController : Controller
+    {
+        public JsonResult KisiEkle(Kisi model)
+        {
+            //
+            return Json("");
+        }
+    }
+}
---
+++ BilgeAdam.UI.Web/Models/VM/Kisi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BilgeAdam.UI.Web.Models.VM
+{
+    public class Kisi
+    {
+        public string Name { get; set; }
+
+        public string SurName { get; set; }
+
+        public string City { get; set; }
+    }
+}
