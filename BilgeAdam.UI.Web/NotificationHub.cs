fe997e89d6322bcc24bf487004a6ac14cc03fef9
cagatay.yildiz <cagatay.yildiz@BSKPC103-YZLM>
2019-05-26 13:21:06 +0300

    signalr entegrasyon

---
+++ BilgeAdam.UI.Web/NotificationHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BilgeAdam.UI.Web
+{
+    public class NotificationHub : Hub
+    {
+        public void Deleteproductalarm(int id)
+        {
+            Clients.Others.productalarm(id);
+        }
+
+        public override Task OnConnected()
+        {
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            return base.OnDisconnected(stopCalled);
+        }
+    }
+}
